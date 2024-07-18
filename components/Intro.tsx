0453a5188f4d3f82a9029b5f91fa3dd443036bfd
Syed Nouman <45708152+noumqn@users.noreply.github.com>
2024-07-18 17:47:49 +0530

    Update Intro.tsx

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -46,7 +46,7 @@ export default function Intro() {
       <div className="mx-auto flex min-h-screen max-w-5xl flex-col items-center justify-center px-10 py-24 text-4xl font-semibold tracking-tight md:py-28 md:text-6xl lg:px-20 lg:py-3 lg:text-7xl">
         <div className="leading-[1.15]">
           <div className="introText" style={{ opacity: opacityForBlock(progress, 0) }}>
-            I love coding.
+            I love designing.
           </div>
           <span
             className="introText inline-block after:content-['_']"
