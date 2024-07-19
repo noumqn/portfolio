cdb3e1a387fe2f551c3863f88f50d18666271ac0
Syed Nouman <45708152+noumqn@users.noreply.github.com>
2024-07-19 16:01:26 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,8 @@ import MainLayout from '@/layouts/MainLayout';
 import { allAuthors } from 'contentlayer/generated';
 
 export const metadata = {
-  title: 'About - Dale Larroder',
-  description: 'About me - Dale Larroder',
+  title: 'About - Syed Nouman',
+  description: 'About me - Syed Nouman',
 };
 
 export default function About() {
